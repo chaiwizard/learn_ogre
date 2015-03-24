ea9a73f795a5afc2d43e6799bbd90dec3590bed0
Chai Qiang <q.chai@iotek.com.cn>
2015-03-24 14:27:41 +0800

    add ogre helloworld example

--- helloogre/main.cpp
+++ helloogre/main.cpp
@@ -1,3 +1,8 @@
+/**
+ *Compile under ubuntu 12.04 
+ *g++ -o ../../dist/bin/app main.cpp `pkg-config --libs OGRE OIS`
+ */
+
 #include "OGRE/ExampleApplication.h"
 
 class Example1 : public ExampleApplication
@@ -5,8 +10,19 @@ class Example1 : public ExampleApplication
 public:
 	void createScene()
 	{
-		Ogre::Entity* ent = mSceneMgr->createEntity("MyEntity","ogrehead.mesh");
-		mSceneMgr->getRootSceneNode()->attachObject(ent);
+		//Set the scene's ambient light
+		mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5f,0.5f,0.5f));
+
+		//Create an Entity
+		Ogre::Entity* ogreHead = mSceneMgr->createEntity("Head","ogrehead.mesh");
+
+		//Create a SceneNode and attach to Entity to it
+		Ogre::SceneNode *headNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("HeadNode");
+		headNode->attachObject(ogreHead);
+
+		//Create a Light and set its position
+		Ogre::Light *light = mSceneMgr->createLight("MainLight");
+		light->setPosition(20,80,50);
 
 	}
 };
